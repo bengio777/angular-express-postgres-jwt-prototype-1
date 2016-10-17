5905cc0ac813f12500991135128ac129cd2815b5
Brooks Patton <brooks@brookzerker.com>
2016-10-16 20:50:48 -0600

    sending new user object to server

--- public/javascripts/login/login.controller.js
+++ public/javascripts/login/login.controller.js
@@ -1,4 +1,21 @@
 angular.module('jwt-prototype')
-.controller('login.controller.js', function ($scope) {
-  
+.controller('login.controller.js', function ($scope, $http) {
+  $scope.create = (form) => {
+    const user = {
+      username: form.createAccount.username,
+      password: form.createAccount.password
+    }
+
+    $http.post('/users/create', user)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  }
+
+  $scope.loginSubmitted = () => {
+    console.log('login form submitted');
+  }
 })
