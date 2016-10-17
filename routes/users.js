8908e557e5bb6b77fb1615d8deee43b7f139f1bb
Brooks Patton <brooks@brookzerker.com>
2016-10-16 21:22:38 -0600

    saving new user to the db

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const userQueries = require('../database/user-queries')
+const bcrypt = require('bcrypt')
 
 router.post('/create', (req, res, next) => {
-  res.send('yo')
+  bcrypt.genSalt(12, (err, salt) => {
+    if(err) return next(err)
+    bcrypt.hash(req.body.password, salt, (err, hash) => {
+      if(err) return next(err)
+
+      const user = {
+        username: req.body.username,
+        password: hash
+      }
+
+      userQueries.create(user)
+        .then((result) => {
+          res.json(result)
+        })
+        .catch((err) => {
+          console.error('There was a problem creating a user');
+          next(err)
+        })
+    })
+  })
 })
 
 module.exports = router;
